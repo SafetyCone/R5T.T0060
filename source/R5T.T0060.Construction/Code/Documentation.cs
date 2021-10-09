ba4d9893f489ea4a785bf0043d5c42e4564de7de
davidcoats <david@getsafetycone.com>
2021-10-09 13:23:41 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0060.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0060.Construction
+{
+    /// <summary>
+    /// Construction project for IPredicateProvider, IModifierProvider, and other high-level function related extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
